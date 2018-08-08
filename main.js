3b05af9fcfc3453b36e0b08548aa17fdcfba0b47
Nguyen Thanh Long <thanhlong123_vietnam@yahoo.com.vn>
2018-08-08 11:19:28 +0700

    update

--- main.js
+++ main.js
@@ -174,7 +174,8 @@
 
         // Adjust compass heading
         var adjustment = 0;
-        var browserOrientation = CompassUtils.getBrowserOrientation();
+        alert("1");
+        var browserOrientation = this.getBrowserOrientation();
         alert(browserOrientation);
         if (typeof browserOrientation !== "undefined") {
             var currentOrientation = browserOrientation.split("-");
