24d02e447f72f554ec1c0a561b6758f08b94a251
Nguyen Thanh Long <thanhlong123_vietnam@yahoo.com.vn>
2018-08-30 09:10:40 +0700

    update quaternion

--- main.js
+++ main.js
@@ -569,7 +569,7 @@
                 // //camera.quaternion.setFromEuler(new T.Euler(T.Math.degToRad(orientation.beta), T.Math.degToRad(orientation.alpha), -T.Math.degToRad(orientation.gamma), 'YXZ'));
                 // //camera.quaternion.multiply(new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
                 var q = T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5));
-                // this.el.object3D.quaternion.copy( q )
+                this.el.object3D.quaternion.copy( q )
             }
         }
     });
