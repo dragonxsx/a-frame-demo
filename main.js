083b8b0169288fe11bf600a41c2ff3d586149071
Nguyen Thanh Long <thanhlong123_vietnam@yahoo.com.vn>
2018-08-30 08:57:28 +0700

    update quaternion

--- main.js
+++ main.js
@@ -560,15 +560,16 @@
         },
 
         tick: function() {
-            if (Date.now() - this.data.timestamp > 100) {
-                this.data.timestamp += 100;
+            if (Date.now() - this.data.timestamp > 1000) {
+                this.data.timestamp += 1000;
                 
                 // TODO: 磁石の北と真北のズレを修正しなくていいのか確認する。
 
-                var camera = document.querySelector(this.data.cameraSelector);
-                //camera.quaternion.setFromEuler(new T.Euler(T.Math.degToRad(orientation.beta), T.Math.degToRad(orientation.alpha), -T.Math.degToRad(orientation.gamma), 'YXZ'));
-                var q = camera.quaternion.multiply(new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
-                this.el.object3D.quaternion.copy( q )
+                // var camera = document.querySelector(this.data.cameraSelector);
+                // //camera.quaternion.setFromEuler(new T.Euler(T.Math.degToRad(orientation.beta), T.Math.degToRad(orientation.alpha), -T.Math.degToRad(orientation.gamma), 'YXZ'));
+                // //camera.quaternion.multiply(new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
+                // var q = new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5));
+                // this.el.object3D.quaternion.copy( q )
             }
         }
     });
